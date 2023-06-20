acf7fac54cfed455e51ac667d0ab2792115fc737
root <root@Host.kh33fgqfae5ufahe2famy5xurh.rx.internal.cloudapp.net>
2023-06-20 14:50:23 +0000

    adding files

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+features {}
+}
+resource "azurerm_resource_group" "k8s" {
+  name     = var.resourcename
+  location = var.location
+}
+resource "azurerm_virtual_network" "k8s" {
+  name                = "k8s-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.k8s.location
+  resource_group_name = azurerm_resource_group.k8s.name
+}
+resource "azurerm_subnet" "k8s" {
+  name                 = "k8s-subnet"
+  resource_group_name  = azurerm_resource_group.k8s.name
+  virtual_network_name = azurerm_virtual_network.k8s.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = var.clustername
+  location            = azurerm_resource_group.k8s.location
+  resource_group_name = azurerm_resource_group.k8s.name
+  dns_prefix          = var.dnspreffix
+default_node_pool {
+    name       = "default"
+    node_count = var.agentnode
+    vm_size    = var.size
+  }
+service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "resourcename" {
+  default = "k8s-resources"
+}
+variable "clustername" {
+  default = "kubernetes-aks1"
+}
+variable "location" {
+  default = "centralindia"
+}
+variable "dnspreffix" {
+  default = "kubecluster"
+}
+variable "size" {
+  default = "Standard_D2_v2"
+}
+variable "agentnode" {
+  default = "1"
+}
+variable "subscription_id" {
+  default = "b3c7ce23-4463-41af-b0ee-2aad6de340b9"
+}
+variable "client_id" {
+  default = "892c5683-828c-45f8-95cb-e112a70d5bd2"
+}
+variable "client_secret" {
+  default = "_AK8Q~DuRTBorGh23XRZH5NQAbDWi.S-FWWrhbfi"
+}
+variable "tenant_id" {
+  default = "2d281bb5-1697-4054-99e8-8ad9ff83402b"
+}
