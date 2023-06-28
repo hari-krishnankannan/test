3beae0e0aa046872f58872c65d9873e809a3b14b
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-06-29 03:44:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,15 +16,3 @@ variable "size" {
 variable "agentnode" {
   default = "1"
 }
-variable "subscription_id" {
-  default = "b3c7ce23-4463-41af-b0ee-2aad6de340b9"
-}
-variable "client_id" {
-  default = "892c5683-828c-45f8-95cb-e112a70d5bd2"
-}
-variable "client_secret" {
-  default = "_AK8Q~DuRTBorGh23XRZH5NQAbDWi.S-FWWrhbfi"
-}
-variable "tenant_id" {
-  default = "2d281bb5-1697-4054-99e8-8ad9ff83402b"
-}
