ec3465491749512383c9495d25c92671c63dbdf1
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-06-29 04:17:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 provider "azurerm" {
 
-data "azurerm_key_vault" "mysecrets" {
+data "azurerm_key_vault" "mysecretsfiles" {
   name                = "mysecretsfiles"
   resource_group_name = "Azurevms"
 }
 data "azurerm_key_vault_secret" "subscriptionIDs" {
   name         = "subscriptionID"
-  key_vault_id = data.azurerm_key_vault.mysecrets.id
+  key_vault_id = data.azurerm_key_vault.mysecretsfiles.id
 }
 data "azurerm_key_vault_secret" "tenantids" {
   name         = "tenantid"
-  key_vault_id = data.azurerm_key_vault.mysecrets.id
+  key_vault_id = data.azurerm_key_vault.mysecretsfiles.id
 }
 data "azurerm_key_vault_secret" "clientids" {
   name         = "clientid"
-  key_vault_id = data.azurerm_key_vault.mysecrets.id
+  key_vault_id = data.azurerm_key_vault.mysecretsfiles.id
 }
 data "azurerm_key_vault_secret" "clientSecrets" {
   name         = "clientSecret"
-  key_vault_id = data.azurerm_key_vault.mysecrets.id
+  key_vault_id = data.azurerm_key_vault.mysecretsfiles.id
 }
   subscription_id = data.azurerm_key_vault_secret.subscriptionID.value
   client_id       = data.azurerm_key_vault_secret.clientid.value
